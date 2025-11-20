ec929ea0c943993ecb89fdef8fd38fbb136c8fd5
Leonardo Aquino <leonardoeaquino@outlook.com>
2025-11-19 21:29:28 -0600

    fix: changed toast postition to bottom center

--- src/app/shared/components/toasts/toast-container.ts
+++ src/app/shared/components/toasts/toast-container.ts
@@ -18,7 +18,10 @@ import { ToastService } from '../../services/toast.service';
     </ngb-toast>
     }
   `,
-  host: { class: 'toast-container position-fixed top-0 end-0 p-3', style: 'z-index: 1200' },
+  host: {
+    class: 'toast-container position-fixed bottom-0 start-50 translate-middle-x p-3 text-center',
+    style: 'z-index: 1200',
+  },
 })
 export class ToastsContainer {
   toastService = inject(ToastService);
